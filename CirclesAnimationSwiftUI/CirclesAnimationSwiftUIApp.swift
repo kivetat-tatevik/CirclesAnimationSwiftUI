dc23c6abbc478a5860c3a7ca28b6529dab0a05c1
kivetat-tatevik <kivetat.85@yandex.ru>
2023-10-31 20:17:50 +0200

    Initial Commit

---
+++ CirclesAnimationSwiftUI/CirclesAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesAnimationSwiftUIApp.swift
+//  CirclesAnimationSwiftUI
+//
+//  Created by Tatevik Khunoyan on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CirclesAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
