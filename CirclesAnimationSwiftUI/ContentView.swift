393f5e97e56bd5ea68f2b709ae879ab3f32158ce
kivetat-tatevik <kivetat.85@yandex.ru>
2023-10-31 20:29:31 +0200

    Done

--- CirclesAnimationSwiftUI/ContentView.swift
+++ CirclesAnimationSwiftUI/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isTapped = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Circle()
+                .scale(self.isTapped ? 0.6 : 1.0)
+                .fill(self.isTapped ? Color(.blue) : .red)
+                .frame(width: 200, height: 200)
+                .animation(.linear, value: isTapped)
+                .onTapGesture{
+                    self.isTapped.toggle()
+                }
+            
+            Circle()
+                .scale(self.isTapped ? 0.3 : 1.0)
+                .fill(self.isTapped ? Color(.red) : .orange)
+                .frame(width: 200, height: 200)
+                .animation(.linear(duration: 2), value: isTapped)
+                .onTapGesture{
+                    self.isTapped.toggle()
+                }
+            
+            Path() { path in
+                path.addEllipse(in: CGRect(x: 0, y: 0, width: 200, height: 200))
+            }
+            .scale(self.isTapped ? 0.5 : 1.0)
+            .fill(self.isTapped ? Color(.green) : .blue)
+            .frame(width: 200, height: 200)
+            .animation(.linear, value: isTapped)
+            .onTapGesture{
+                self.isTapped.toggle()
+            }
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
